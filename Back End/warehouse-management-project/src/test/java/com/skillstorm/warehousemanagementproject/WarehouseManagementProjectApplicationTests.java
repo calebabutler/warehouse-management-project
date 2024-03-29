3ede471313af02610b58a2195f66638394ab1edc
Caleb Butler <butlercq@gmail.com>
2024-03-29 16:05:44 -0400

    First commit with initial front end and back end

---
+++ Back End/warehouse-management-project/src/test/java/com/skillstorm/warehousemanagementproject/WarehouseManagementProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillstorm.warehousemanagementproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManagementProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
