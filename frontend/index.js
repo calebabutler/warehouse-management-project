8847a5a3841ab42c486c44e2063020d61c2e294b
Caleb Butler <butlercq@gmail.com>
2024-04-03 18:00:55 -0400

    Improve frontend with a few buttons

--- frontend/index.js
+++ frontend/index.js
@@ -9,10 +9,12 @@ const addWarehousesToDom = () => {
         const item = document.createElement('div');
         item.className = 'accordion-item';
         item.id = 'warehouse' + warehouse.id;
+        root.appendChild(item);
 
         const header = document.createElement('h2');
         header.className = 'accordion-header';
         header.id = 'warehouse' + warehouse.id + 'header';
+        item.appendChild(header);
 
         const button = document.createElement('button');
         button.className = 'accordion-button collapsed';
@@ -23,23 +25,65 @@ const addWarehousesToDom = () => {
         button.setAttribute('aria-expanded', 'false');
         button.setAttribute('aria-controls', 'warehouse' + warehouse.id + 'collapse');
         button.innerText = warehouse.name;
+        header.appendChild(button);
 
         const collapse = document.createElement('div');
         collapse.className = 'accordion-collapse collapse';
         collapse.id = 'warehouse' + warehouse.id + 'collapse';
         collapse.setAttribute('data-bs-parent', '#warehouseList');
+        item.appendChild(collapse);
 
         const body = document.createElement('div');
-        body.className = 'accordion-body';
+        body.className = 'accordion-body d-grid gap-2';
         body.id = 'warehouse' + warehouse.id + 'body';
-        body.innerText = warehouse.description;
-
-        header.appendChild(button);
-        item.appendChild(header);
         collapse.appendChild(body);
-        item.appendChild(collapse);
-        root.appendChild(item);
+
+        const row = document.createElement('div');
+        row.className = 'row align-items-center';
+        row.id = 'warehouse' + warehouse.id + 'row';
+        body.appendChild(row);
+
+        const description = document.createElement('div');
+        description.className = 'col-8';
+        description.id = 'warehouse' + warehouse.id + 'description';
+        description.innerText = warehouse.description;
+        row.appendChild(description);
+
+        const buttonGroup = document.createElement('div');
+        buttonGroup.className = 'col-4 btn-group';
+        buttonGroup.role = 'group';
+        buttonGroup.setAttribute('aria-label', 'Edit or Delete?');
+        row.appendChild(buttonGroup);
+
+        const editButton = document.createElement('button');
+        editButton.type = 'button';
+        editButton.className = 'btn btn-primary btn';
+        editButton.id = 'warehouse' + warehouse.id + 'editButton';
+        editButton.innerText = 'Edit';
+        buttonGroup.appendChild(editButton);
+
+        const deleteButton = document.createElement('button');
+        deleteButton.type = 'button';
+        deleteButton.className = 'btn btn-primary btn';
+        deleteButton.id = 'warehouse' + warehouse.id + 'deleteButton';
+        deleteButton.innerText = 'Delete';
+        buttonGroup.appendChild(deleteButton);
+
+        const addProductsRow = document.createElement('div');
+        addProductsRow.className = 'row';
+        addProductsRow.id = 'warehouse' + warehouse.id + 'addProductsRow';
+        body.appendChild(addProductsRow);
+
+        const addProducts = document.createElement('button');
+        addProducts.className = 'btn btn-dark';
+        addProducts.id = 'warehouse' + warehouse.id + 'addProducts';
+        addProducts.innerText = 'Add a Product';
+        addProductsRow.appendChild(addProducts);
+
     }
+};
+
+const addProductsToDom = () => {
 };
 
 const updateDom = () => {
