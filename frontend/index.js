b5330ed8c8ac90ce704d535489d99701d7785247
Caleb Butler <butlercq@gmail.com>
2024-04-03 22:25:58 -0400

    Add product table to frontend

--- frontend/index.js
+++ frontend/index.js
@@ -69,6 +69,42 @@ const addWarehousesToDom = () => {
         deleteButton.innerText = 'Delete';
         buttonGroup.appendChild(deleteButton);
 
+        const productsTable = document.createElement('table');
+        productsTable.className = 'table';
+        productsTable.id = 'warehouse' + warehouse.id + 'productsTable';
+        body.appendChild(productsTable);
+
+        const productsHead = document.createElement('thead');
+        productsHead.id = 'warehouse' + warehouse.id + 'productsHead';
+        productsTable.appendChild(productsHead);
+
+        const headerRow = document.createElement('tr');
+        productsHead.appendChild(headerRow);
+
+        const idHeader = document.createElement('th');
+        idHeader.innerText = 'Product id';
+        headerRow.appendChild(idHeader);
+
+        const typeHeader = document.createElement('th');
+        typeHeader.innerText = 'Type';
+        headerRow.appendChild(typeHeader);
+
+        const descriptionHeader = document.createElement('th');
+        descriptionHeader.innerText = 'Description';
+        headerRow.appendChild(descriptionHeader);
+
+        const categoryHeader = document.createElement('th');
+        categoryHeader.innerText = 'Category';
+        headerRow.appendChild(categoryHeader);
+
+        const optionsHeader = document.createElement('th');
+        optionsHeader.innerText = 'Options';
+        headerRow.appendChild(optionsHeader);
+
+        const products = document.createElement('tbody');
+        products.id = 'warehouse' + warehouse.id + 'products';
+        productsTable.append(products);
+
         const addProductsRow = document.createElement('div');
         addProductsRow.className = 'row';
         addProductsRow.id = 'warehouse' + warehouse.id + 'addProductsRow';
@@ -84,10 +120,56 @@ const addWarehousesToDom = () => {
 };
 
 const addProductsToDom = () => {
+    for (const product of database['products']) {
+        const root = document.getElementById('warehouse' + product.warehouse.id + 'products');
+
+        const row = document.createElement('tr');
+        root.appendChild(row);
+
+        const idElement = document.createElement('td');
+        idElement.innerText = product.id;
+        row.appendChild(idElement);
+
+        const typeElement = document.createElement('td');
+        typeElement.innerText = product.type.name;
+        row.appendChild(typeElement);
+
+        const descriptionElement = document.createElement('td');
+        descriptionElement.innerText = product.type.description;
+        row.appendChild(descriptionElement);
+
+        const categoryElement = document.createElement('td');
+        categoryElement.innerText = product.type.category.name;
+        row.appendChild(categoryElement);
+
+        const buttonColumn = document.createElement('td');
+        row.appendChild(buttonColumn);
+
+        const buttonGroup = document.createElement('div');
+        buttonGroup.className = 'col-4 btn-group';
+        buttonGroup.role = 'group';
+        buttonGroup.setAttribute('aria-label', 'Edit or Delete?');
+        buttonColumn.appendChild(buttonGroup);
+
+        const editButton = document.createElement('button');
+        editButton.type = 'button';
+        editButton.className = 'btn btn-primary btn';
+        editButton.id = 'product' + product.id + 'editButton';
+        editButton.innerText = 'Edit';
+        buttonGroup.appendChild(editButton);
+
+        const deleteButton = document.createElement('button');
+        deleteButton.type = 'button';
+        deleteButton.className = 'btn btn-primary btn';
+        deleteButton.id = 'product' + product.id + 'deleteButton';
+        deleteButton.innerText = 'Delete';
+        buttonGroup.appendChild(deleteButton);
+    }
 };
 
 const updateDom = () => {
     addWarehousesToDom();
+    addProductsToDom();
 };
 
 document.addEventListener('DOMContentLoaded', () => {
