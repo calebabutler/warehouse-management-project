4a54824bfc5ca5c17e8dc0de5c22e9738a37af67
Caleb Butler <butlercq@gmail.com>
2024-04-05 11:14:09 -0400

    Make the data better suited for a demo

--- data_generator.py
+++ data_generator.py
@@ -2,10 +2,10 @@
 import random
 
 def main():
-    starting_value = 201
+    starting_value = 11
     warehouse_value = 3
     insert_statements = []
-    for i in range(100):
+    for i in range(5):
         for j in range(random.randint(1, 5)):
             insert_statements.append(f'insert into PRODUCTS (product_type_id, warehouse_id) values ({starting_value + i}, {warehouse_value});')
     random.shuffle(insert_statements)
